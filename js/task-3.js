47d2a498e68910f39e7a671d3d96f055c04f1f71
Viktor Ignatiev <viktor.ignatiev@wp.pl>
2024-03-10 20:39:17 +0100

    Home Work III vol I

---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function filterArray(numbers, value) {
+    let finalArray = [];
+    for (const element of numbers) {
+        if (element > value) {
+           finalArray.push(element);
+        }
+    }
+        return finalArray
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
+
